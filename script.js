d91a23b0fdef689b1c90990fb3e4f265272b2787
mcpalta <mauriciocatalanmedina@gmail.com>
2025-05-18 15:50:40 -0400

    Style – estilos implementados en proyecto.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const students=[];
 
-const tableBody=document.querySelector("#studentsTable tbody")
+const tableBody=document.querySelector("#studentsTable tbody");
+const averageDiv=document.getElementById("average");
 
 document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
 
     const name=document.getElementById("name").value.trim();
     const lastName=document.getElementById("lastName").value.trim();
-    const grade=document.getElementById("grade").value.trim();
+    const grade = parseFloat(document.getElementById("grade").value.trim());
     const date=document.getElementById("date").value.trim();
 
     if(grade <1 || grade >7 || !name || !lastName || isNaN(grade)){
@@ -20,6 +21,8 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
     const student={name,lastName,grade,date};
     students.push(student);
     addStudentToTable(student);
+    calcularPromedio()
+    
 
     console.log(students)
     this.reset();
@@ -36,3 +39,16 @@ function addStudentToTable(student){
         
     tableBody.appendChild(row);   
 }
+
+function calcularPromedio(){
+    if(students.length===0){
+       averageDiv.textContent="Promedio General del Curso:N/A"
+        return;
+    }
+    const total=students.reduce((sum,s)=>sum+s.grade,0);
+    console.log(total)
+    const average=total/students.length;
+    console.log(average)
+    averageDiv.textContent=`Promedio General del Curso: ${average.toFixed(2)}`;
+} 
+
